1eb533331b72d238b5c15c047d011e8496a8a03c
unknown <v-datare@microsoft.com>
2018-05-23 12:36:07 +0000

    ttt

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-
 var databaseUrl = 'mongodb://best-for-you-db-v-datare:qVlo13cyPXiEsKVsPrRZv9aQbJrnePyulaVZb6QPRWw6TD6xg9UeUOndAmACCktlsT284sTqi7Rnq4ygYJz8Cw==@best-for-you-db-v-datare.documents.azure.com:10255/best-for-you-db-v-datare?ssl=true&replicaSet=globaldb';
 
 var mongoose = require('mongoose');
